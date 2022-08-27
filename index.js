7044495419d847899a433e1ef6f270e076ba9ca8
Queryzi <56193446+Queryzi@users.noreply.github.com>
2022-08-27 14:58:52 -0400

    Test commit

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from "express";
 import cors from 'cors';
 
 const app = express();
+const api = require('./api/api.js')
 
 app.use("/api", )
 
 app.listen(3000, () => {
     console.log('Express server has loaded.')
-})
+});
